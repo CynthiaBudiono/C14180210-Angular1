102a12591d78b1257b7c996b1d0871f526cd58da
CynthiaBudiono <c14180210@john.petra.ac.id>
2021-02-23 21:04:42 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  // index = 0;
+
+  // board: any[4][4];
+  board = [
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"],
+    ["*", "*", "*", "*", "*"]
+  ];
+
+  baris = 1;
+  kolom = 1;
+
+  pemain: boolean = true;
+
+  // papan() {
+  //   for (let i = 0; i < this.board.length; i++) {
+  //     for (let j = 0; j < this.board.length; j++) {
+  //       "tesss";
+  //     }
+  //   }
+  // }
+
+  play(r, c) {
+    if (this.pemain == true) {
+      this.board[r][c] = "0";
+      this.pemain = false;
+    } else {
+      this.board[r][c] = "1";
+      this.pemain = true;
+    }
+  }
 }
