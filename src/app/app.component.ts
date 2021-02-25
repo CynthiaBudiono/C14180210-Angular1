f87d60cbb5e988866fb23cb612c8253a31fdf764
CynthiaBudiono <c14180210@john.petra.ac.id>
2021-02-25 20:06:24 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,91 +20,88 @@ export class AppComponent {
   baris = 1;
   kolom = 1;
   pemain: boolean = true;
-
-  hasilP0 = 0;
-  hasilP1 = 0;
-
+  flag: boolean;
+  hasil = 0;
+  choice: HTMLLabelElement;
   win = 0;
-  // papan() {
-  //   for (var i: number = 0; i < 5; i++) {
-  //     this.output[i] = [];
-  //     for (var j: number; j < 5; j++) {
-  //       this.output[i][j] = "*";
-  //     }
-  //   }
-  // }
+  huruf: String;
 
   play(r, c) {
-    // r = r - 1;
-    // c = c - 1;
-    if (r < 1 || r > 5) {
+    r = r - 1;
+    c = c - 1;
+    if (r < 0 || r > 6) {
       alert("Baris Melebihi Papan Board");
-    } else if (c < 1 || c > 5) {
+    } else if (c < 0 || c > 6) {
       alert("Kolom Melebihi Papan Board");
     } else {
       // console.log(r + " " + c);
       // console.log(this.board[r][c]);
-      if (this.board[r - 1][c - 1] == "*") {
+      if (this.board[r][c] == "*") {
         if (this.pemain == true) {
-          this.board[r - 1][c - 1] = "0";
-          this.pemain = false;
+          this.board[r][c] = "0";
         } else {
-          this.board[r - 1][c - 1] = "1";
-          this.pemain = true;
+          this.board[r][c] = "1";
         }
       } else {
         console.log("pilih yang lain");
+        alert("pilih yang lain");
+        this.choice = document.getElementById("chc") as HTMLLabelElement;
+        this.pemain = !this.pemain;
       }
     }
 
     //Horizontal
+    this.hasil = 0;
+    if (this.pemain == true) {
+      this.huruf = "0";
+    } else {
+      this.huruf = "1";
+    }
+    this.flag = true;
     for (let i = 0; i < 5; i++) {
-      this.hasilP0 = 0;
-      this.hasilP1 = 0;
-      for (let j = 0; j < 5; j++) {
-        // console.log(this.board[i][j]);
-        if (this.board[i][j] == "0") {
-          this.hasilP0 += 1;
+      if (this.board[r][i] == this.huruf && this.flag == true) {
+        this.hasil += 1;
+      } else {
+        if (this.hasil > 0) {
+          this.flag = false;
         }
-        if (this.board[i][j] == "1") {
-          this.hasilP1 += 1;
+      }
+    }
+    console.log("Hasil : " + this.hasil);
+
+    if (this.hasil >= 4) {
+      if (this.pemain == true) {
+        console.log("Player 0 WIN !!");
+        this.win = 1;
+      } else {
+        console.log("Player 1 WIN !!");
+        this.win = 2;
+      }
+    } else {
+      //Vertikal
+      this.hasil = 0;
+      this.flag = true;
+      for (let i = 0; i < 5; i++) {
+        if (this.board[i][c] == this.huruf && this.flag == true) {
+          this.hasil += 1;
+        } else {
+          if (this.hasil > 0) {
+            this.flag = false;
+          }
         }
-        // console.log("hasil P0 : " + this.hasilP0);
-        // console.log("hasil P1 : " + this.hasilP1);
-        if (this.hasilP0 >= 4) {
+      }
+
+      if (this.hasil >= 4) {
+        if (this.pemain == true) {
           console.log("Player 0 WIN !!");
           this.win = 1;
-        }
-        if (this.hasilP1 >= 4) {
+        } else {
           console.log("Player 1 WIN !!");
           this.win = 2;
         }
       }
-      // console.log("\n");
     }
 
-    //Vertikal
-    for (let i = 0; i < 5; i++) {
-      this.hasilP0 = 0;
-      this.hasilP1 = 0;
-      for (let j = 0; j < 5; j++) {
-        if (this.board[j][i] == "0") {
-          this.hasilP0 += 1;
-        }
-        if (this.board[j][i] == "1") {
-          this.hasilP1 += 1;
-        }
-        // console.log("hasil P0 : " + this.hasilP0);
-        // console.log("hasil P1 : " + this.hasilP1);
-        if (this.hasilP0 >= 4) {
-          console.log("Player 0 WIN !!");
-          this.win = 1;
-        }
-        if (this.hasilP1 >= 4) {
-          console.log("Player 1 WIN !!");
-          this.win = 2;
-        }
-      }
-    }
+    this.pemain = !this.pemain;
   }
 }
